c0341d390dce11ed101d675cb50cc0a306ff47fc
eyemono <eyemono.rin@gmail.com>
2023-01-22 00:14:32 +0000

    :eye: make buttons easy to see

--- src/colors/editor/button.ts
+++ src/colors/editor/button.ts
@@ -3,10 +3,10 @@ import type { EditorColorGenerator } from "../../types/editorColors";
 export const button: EditorColorGenerator = (palette) => {
   return {
     button: {
-      background: palette.bgLight,
-      foreground: palette.fgShadow,
-      secondaryBackground: palette.bgLight,
-      secondaryForeground: palette.fgShadow,
+      background: palette.purple,
+      foreground: palette.bgDarker,
+      secondaryBackground: palette.fgShadow,
+      secondaryForeground: palette.bgDarker,
     },
   };
 };
